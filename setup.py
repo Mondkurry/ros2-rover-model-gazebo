cec1670b1813130d734a567cfcd7c9a778f25b48
Mondkurry <aryan.mondkar@gmail.com>
2023-07-15 14:41:24 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'rover'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.py'))      # Add path for launch file
+        (os.path.join('share', package_name), glob('urdf/*.py'))        # Add path for urdf
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mondkurry',
+    maintainer_email='aryan.mondkar@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
