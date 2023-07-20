19a21408b017d2bb2d48f472c68937a9f926ee08
Aryan Mondkar <aryan.mondkar@gmail.com>
2023-07-19 22:33:37 -0700

    obstacle avoidance works!

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'obstacle avoider = rover.obstacle_avoidance:main',
+            'obstacle avoider = rover.2_obstacle_avoidance:main',
         ],
     },
 )
